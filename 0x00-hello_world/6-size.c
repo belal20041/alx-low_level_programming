94c7bc26a9aff5348492a99987143676bd9d155c
belal20041 <blalkhys2004@gmail.com>
2023-09-08 23:13:32 +0000

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of char: %c byte\n")
+	printf("size of int: %d byte\n")
+	printf("size of long int: %ld byte\n")
+	printf("size of float: %f byte\n")
+	return (0)
+}
