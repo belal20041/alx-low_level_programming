509e5dcbdd1f7cb34df11c8495c0f2829174a796
belal20041 <belalkhys2004@gmail.com>
2023-09-13 13:08:04 +0000

    me

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 	char CH = 'A';
 
-        while (ch <= 'z')
-        {
-                putchar(ch);
-                ch++;
-        }
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
 	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
