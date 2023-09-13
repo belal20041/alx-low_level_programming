50efb05fb0f894c604f3bd63f0d5f25eb6925449
belal20041 <belalkhys2004@gmail.com>
2023-09-13 14:12:14 +0000

    me

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * description prints the alphabet in lowercase, followed by a new line
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
