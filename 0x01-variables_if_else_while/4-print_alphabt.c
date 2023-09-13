f42e8a35b234e2b096d4b866bc22c26ef8f1daca
belal20041 <belalkhys2004@gmail.com>
2023-09-13 13:32:09 +0000

    me

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z' && ch != 'q' && ch != 'e')
+	while (ch <= 'z')
 	{
 		if (ch == 'e' || ch == 'q')
 		{
 			ch++;
-		}	
+		}
 		putchar(ch);
 		ch++;
 	}
