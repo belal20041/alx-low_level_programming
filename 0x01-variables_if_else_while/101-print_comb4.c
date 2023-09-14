ae52da9c22ecc2938a3ed2f459162d0ea607e9f9
belal20041 <belalkhys2004@gmail.com>
2023-09-14 09:50:51 +0000

    me

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,9 @@
 
 int main(void)
 {
-	int i, j, k;
+	int i;
+	int j;
+	int k;
 
 	for (i = '0'; i <= '7'; i++)
 	{
@@ -26,7 +28,7 @@ int main(void)
 				}
 			}
 		}
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
