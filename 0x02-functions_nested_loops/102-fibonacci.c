343d6738bfc730c370645ea4b31a23ba0ac01777
belal20041 <belalkhys2004@gmail.com>
2023-09-21 22:21:36 +0000

    me

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x = 0, y = 1, sum, count;
+
+	for (count = 0; count <= 49; count++)
+	{
+		sum x + y;
+		printf("lu", sum);
+
+		x = y;
+		y = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+}
