e307d7b7cce604c614bb13fdf44e0eeb868ab24b
belal20041 <belalkhys2004@gmail.com>
2023-09-21 17:03:31 +0000

    me

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,19 @@
 
 int print_sign(int n)
 {
+	if (n > 0)
+	{
+		_putchar(43);
+		 return (1);
+	}
 	if (n == 0)
 	{
-		if (n > 0)
-			return (1);
-		else
-			return (-1);
+		_putchar(48);
+		return (0);
 	}
-	return (0);
+	else
+	{
+		_putchar(45)
+		return (-1);
+	}
 }
