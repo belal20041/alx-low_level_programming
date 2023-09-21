f25ecbb82b6016fc185c9bf489634b311217779c
belal20041 <belalkhys2004@gmail.com>
2023-09-21 21:31:02 +0000

    me

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,25 +9,32 @@ void print_times_table(int n)
 {
 	int x, y, res;
 
-	for (x = 0; x < n; x++)
+	for (x = 0; x <= n; x++)
 	{
 		if (n > 15 && n < 0)
 			break;
 		_putchar(48);
-		for (y = 1; y < n; y++)
+		for (y = 1; y <= n; y++)
 		{
 			res = x * y;
 
-			if (y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
 
-			if (res > 9)
-				_putchar((res % 10) + 48);
-			_putchar((res / 10) + 48);
+			if (res <= 9)
+				_putchar(' ');
+			if (res <= 99)
+				_putchar(' ');
+
+			if (res >= 100)
+			{
+				_putchar((res / 100) + 48);
+				_putchar((res / 10) % 10 + 48);
+			} else if (res <= 99 && res >= 10)
+				 _putchar((res / 10) + 48);
+			 _putchar((res / 10) + 48);
 		}
+		 _putchar('\n');
 	}
 
 }
