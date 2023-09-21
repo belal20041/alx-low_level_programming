8bb2ae4642b642759fa32e9f90235ae67f950bd2
belal20041 <belalkhys2004@gmail.com>
2023-09-21 16:43:51 +0000

    me

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * print_sign -prints the sign of a number.
- * @n: number to check
- * Return: 1 and prints + if n is greater than zero
- *         0 and prints 0 if n is zero
- *         -1 and prints - if n is less than zero
+ * _isalpha -prints the sign of a number.
+ * @c: number to check
+ * Return: 1 1 if c is a letter, lowercase or uppercase
+ *         0 otherwise
  */
 
-int print_sign(int n)
+int _isalpha(int c)
 {
-	if (n == 0)
-	{
-		if (n > 0)
-			return (1);
-		else
-			return (-1);
-	}
+	if (c >= 65 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
+		return (1);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign -prints the sign of a number.
+ * @n: number to check
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *         -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		if (n > 0)
+			return (1);
+		else
+			return (-1);/usr/bin/ld: /tmp/ccl37YrW.o: in function `test_isalpha':
+main_0.c:(.text+0x15): undefined reference to `_isalpha'
+collect2: error: ld returned 1 exit status
+	}
+	return (0);
+}
