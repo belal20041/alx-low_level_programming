a8fc68601dd0c25b77cab5170a864c8c6ebd001a
belal20041 <belalkhys2004@gmail.com>
2023-09-22 22:44:00 +0300

    me i am

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -prints 10 times the numbers, from 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int n, m, k;
+
+	for (m = 1; m <= 10; m++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			n = k;
+
+			if (n > 9)
+			{
+				_putchar(1 + 48);
+				n = k 5 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
