0cf68b4266a377834bee8801f7d9ad5489555e9b
belal20041 <belalkhys2004@gmail.com>
2023-09-22 22:05:26 +0300

    me i am

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9Do not print 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+		if (n == 2 || n == 4)
+			continue;
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
