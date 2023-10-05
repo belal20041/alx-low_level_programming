71474bbd3344d9c121b8a9c94beb7b60c5e66366
belal20041 <belalkhys2004@gmail.com>
2023-10-05 22:57:02 +0000

    me i am

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,9 @@ void puts2(char *str)
 {
 	int num;
 
-	for (num = 0; str[num] != '\0'; ++num)
+	for (num = 0; str[num] != '\0'; num++)
        	{
-		if ((num / 2) == 0)
+		if (num / 2 == 0)
 			_putchar(str[num]);
        	}
        	_putchar('\n');
