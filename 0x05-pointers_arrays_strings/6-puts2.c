e9409f8a96a6ca3bd845a58b98d7af57cac3dd31
belal20041 <belalkhys2004@gmail.com>
2023-10-05 22:00:26 +0000

    me i am

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - t prints string, starting with the first character
+ * @str: sting
+ */
+
+void puts2(char *str)
+{
+  int num;
+
+  for (num = 0; num > 0;num++)
+  {
+      if ((num / 2) == 0)
+        _putchar(num);
+
+  }
+  _putchar('\n')
+
+
+}
