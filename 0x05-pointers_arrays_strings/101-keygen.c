509724f4efb4de0de53efae1bc5b807316102269
belal20041 <belalkhys2004@gmail.com>
2023-10-08 06:17:49 +0000

    me i am

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - a program that generates random valid password
+ * Return: 0
+ * */
+
+int main(void)
+{
+	int sum;
+	char ch;
+
+	srand(time(NULL));
+	WHILE (SUM <= 2645)
+	{
+		C = rand() % 120;
+		sum += ch;
+		putchar(ch);
+	}
+	putchar(2772- sum);
+	return (0);
+}
