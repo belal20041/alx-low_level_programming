e44ed7bb094a561c7f06f3012ab035dd350ab000
belal20041 <belalkhys2004@gmail.com>
2023-10-08 20:25:36 +0000

    me i am

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,12 @@
 char *_strcat(char *dest, char *src)
 {
   int ch, ch2;
-  int ch = 0;
+
+  ch = 0;
   while (dest[ch])
     ch++;
 
   for (ch2 = 0; src[ch2]; ch2++)
     dest[ch++] = src[ch2];
-
+  return (dest);
 }
