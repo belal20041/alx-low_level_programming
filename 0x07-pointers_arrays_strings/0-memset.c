7912af06f9058745d2402d04487d2716e44f0a3f
belal20041 <belalkhys2004@gmail.com>
2023-10-13 19:51:18 +0000

    me i am

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,14 @@
  * @n:
  */
 
-char *_memset(char *s, char b, unsigned int n)
-{
-  unsigned int chr;
 
-  for (chr = 0; n > 0; chr++, n--)
-    s[chr] = b;
-  return (0);
+char *_memset(char* s, char b, unsigned int n) 
+{
+  for (unsigned int i = 0; i < n; i++)
+  {
+    *(s + i) = b;
+  }
+  return s;
 }
+
+
