355a279e50d73cc58a86059b29f12f8f92e91e3a
belal20041 <belalkhys2004@gmail.com>
2023-10-19 22:10:56 +0000

    me i am

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -27,7 +27,7 @@ int inception(char *s1, char *s2)
 	if (*s1 == 0)
 		return (0);
 	if (*s1 == 0)
-		ret += wildcmp(s1 + 1, s2 + 1;
+		ret += wildcmp(s1 + 1, s2 + 1);
 	ret += inception(s1 + 1, s2);
 	return (ret);
 }
