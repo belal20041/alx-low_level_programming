d3a1b745c6724775eab31567993e5c6fd9a13e39
belal20041 <belalkhys2004@gmail.com>
2023-10-19 10:48:31 +0000

    me i am

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-  int length = 0;
-  if (*s != '\0')
-    length += _strlen_recursion(s + 1) + 1;
-  return (length);
+	int length = 0;
+	if (*s != '\0')
+	length += _strlen_recursion(s + 1) + 1;
+	return (length);
 }
