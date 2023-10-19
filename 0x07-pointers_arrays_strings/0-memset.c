c519cb8abd28726d63e2957747acfc04ed137180
belal20041 <belalkhys2004@gmail.com>
2023-10-19 10:28:15 +0000

    me i am

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 
 /**
  * _memsset:  fills memory with a constant byte
- * @b:
- * @s:
- * @n:
+ * @b: ef
+ * @s: 	f
+ * @n:c
  */
 
 
 char *_memset(char* s, char b, unsigned int n) 
 {
-  for (unsigned int i = 0; i < n; i++)
-  {
-    *(s + i) = b;
-  }
-  return s;
+	for (unsigned int i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+ 	return s;
 }
 
 
